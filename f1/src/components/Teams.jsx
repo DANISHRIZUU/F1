78b58ddc15fd3d6003e946c10f9fbbebc2f34d59
DANISHRIZUU <danishrizwan621@gmail.com>
2025-09-19 17:53:47 +0500

    nothing changed nothing new

--- f1/src/components/Teams.jsx
+++ f1/src/components/Teams.jsx
@@ -17,12 +17,13 @@ export default function Teams() {
                     <img className='team-img' src='https://media.formula1.com/image/upload/c_lfill,h_224/q_auto/d_common:f1:2025:fallback:car:2025fallbackcarright.webp/v1740000000/common/f1/2025/mclaren/2025mclarencarright.webp'/>
                     
                 </div>
-                <div className='team-card' id='ferrari1'>
-                    <h4>Ferrari</h4>
+                <div className='team-card' id='mclaren1'>
+                    <h4>McLaren</h4>
                     <p>Oscar Pisatri</p>
                     <p>Lando Norris</p>
-                    <img src='https://media.formula1.com/image/upload/c_lfill,w_48/q_auto/v1740000000/common/f1/2025/mclaren/2025mclarenlogowhite.webp'/>
+                    <img src='"https://media.formula1.com/image/upload/c_lfill,w_48/q_auto/v1740000000/common/f1/2025/mclaren/2025mclarenlogowhite.webp'/>
                     <img className='team-img' src='https://media.formula1.com/image/upload/c_lfill,h_224/q_auto/d_common:f1:2025:fallback:car:2025fallbackcarright.webp/v1740000000/common/f1/2025/ferrari/2025ferraricarright.webp'/>
+                    
                 </div>
             </div>
         </div>
