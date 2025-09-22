8adec033cb23c7ac65b4ec5db2aebbd6a7f9037a
DANISHRIZUU <danishrizwan621@gmail.com>
2025-09-22 14:21:56 +0500

    some changes

--- f1/src/components/Teams.jsx
+++ f1/src/components/Teams.jsx
@@ -10,20 +10,55 @@ export default function Teams() {
             <h1>F1 Teams 2025</h1>
             <div className='team-section'>
                 <div className='team-card' id='mclaren1'>
+                    <img className='team-logo' src='https://media.formula1.com/image/upload/c_lfill,w_48/q_auto/v1740000000/common/f1/2025/mclaren/2025mclarenlogowhite.webp'/>
                     <h4>McLaren</h4>
-                    <p>Oscar Pisatri</p>
-                    <p>Lando Norris</p>
-                    <img src='"https://media.formula1.com/image/upload/c_lfill,w_48/q_auto/v1740000000/common/f1/2025/mclaren/2025mclarenlogowhite.webp'/>
                     <img className='team-img' src='https://media.formula1.com/image/upload/c_lfill,h_224/q_auto/d_common:f1:2025:fallback:car:2025fallbackcarright.webp/v1740000000/common/f1/2025/mclaren/2025mclarencarright.webp'/>
                     
                 </div>
-                <div className='team-card' id='mclaren1'>
-                    <h4>McLaren</h4>
-                    <p>Oscar Pisatri</p>
-                    <p>Lando Norris</p>
-                    <img src='"https://media.formula1.com/image/upload/c_lfill,w_48/q_auto/v1740000000/common/f1/2025/mclaren/2025mclarenlogowhite.webp'/>
+                <div className='team-card' id='ferrari1'>
+                    <img className='team-logo' src='https://media.formula1.com/image/upload/c_fit,h_64/q_auto/v1740000000/common/f1/2025/ferrari/2025ferrarilogolight.webp'/>
+                    <h4>Ferrari</h4>                    
                     <img className='team-img' src='https://media.formula1.com/image/upload/c_lfill,h_224/q_auto/d_common:f1:2025:fallback:car:2025fallbackcarright.webp/v1740000000/common/f1/2025/ferrari/2025ferraricarright.webp'/>
-                    
+                </div>
+                <div className='team-card' id='mercedes1'>
+                    <img className='team-logo' src='https://media.formula1.com/image/upload/c_lfill,w_48/q_auto/v1740000000/common/f1/2025/mercedes/2025mercedeslogowhite.webp'/>
+                    <h4>Mercedes</h4>
+                    <img className='team-img' src='https://media.formula1.com/image/upload/c_lfill,h_224/q_auto/d_common:f1:2025:fallback:car:2025fallbackcarright.webp/v1740000000/common/f1/2025/mercedes/2025mercedescarright.webp'/>
+                </div>
+                <div className='team-card' id='redbull1'>
+                    <img className='team-logo' src='https://media.formula1.com/image/upload/c_lfill,w_48/q_auto/v1740000000/common/f1/2025/redbullracing/2025redbullracinglogowhite.webp'/>
+                    <h4>RedBull Racing</h4>
+                    <img className='team-img' src='https://media.formula1.com/image/upload/c_lfill,h_224/q_auto/d_common:f1:2025:fallback:car:2025fallbackcarright.webp/v1740000000/common/f1/2025/redbullracing/2025redbullracingcarright.webp'/>
+                </div>
+                <div className='team-card' id='williams1'>
+                    <img className='team-logo' src='https://media.formula1.com/image/upload/c_lfill,w_48/q_auto/v1740000000/common/f1/2025/williams/2025williamslogowhite.webp'/>
+                    <h4>Williams</h4>
+                    <img className='team-img' src='https://media.formula1.com/image/upload/c_lfill,h_224/q_auto/d_common:f1:2025:fallback:car:2025fallbackcarright.webp/v1740000000/common/f1/2025/williams/2025williamscarright.webp'/>                    
+                </div>
+                <div className='team-card' id='aston1'>
+                    <img className='team-logo' src='https://media.formula1.com/image/upload/c_lfill,w_48/q_auto/v1740000000/common/f1/2025/astonmartin/2025astonmartinlogowhite.webp'/>
+                    <h4>Aston Martin</h4>
+                    <img className='team-img' src='https://media.formula1.com/image/upload/c_lfill,h_224/q_auto/d_common:f1:2025:fallback:car:2025fallbackcarright.webp/v1740000000/common/f1/2025/astonmartin/2025astonmartincarright.webp'/>                    
+                </div>
+                <div className='team-card' id='bulls'>
+                    <img className='team-logo' src='https://media.formula1.com/image/upload/c_lfill,w_48/q_auto/v1740000000/common/f1/2025/racingbulls/2025racingbullslogowhite.webp'/>
+                    <h4>Racing Bulls</h4>
+                    <img className='team-img' src='https://media.formula1.com/image/upload/c_lfill,h_224/q_auto/d_common:f1:2025:fallback:car:2025fallbackcarright.webp/v1740000000/common/f1/2025/racingbulls/2025racingbullscarright.webp'/>                    
+                </div>
+                <div className='team-card' id='kick'>
+                    <img className='team-logo' src='https://media.formula1.com/image/upload/c_lfill,w_48/q_auto/v1740000000/common/f1/2025/kicksauber/2025kicksauberlogowhite.webp'/>
+                    <h4>Kick Sauber</h4>
+                    <img className='team-img' src='https://media.formula1.com/image/upload/c_lfill,h_224/q_auto/d_common:f1:2025:fallback:car:2025fallbackcarright.webp/v1740000000/common/f1/2025/kicksauber/2025kicksaubercarright.webp'/>                    
+                </div>
+                <div className='team-card' id='haas1'>
+                    <img className='team-logo' src='https://media.formula1.com/image/upload/c_lfill,w_48/q_auto/v1740000000/common/f1/2025/haas/2025haaslogowhite.webp'/>
+                    <h4>Haas</h4>
+                    <img className='team-img' src='https://media.formula1.com/image/upload/c_lfill,h_224/q_auto/d_common:f1:2025:fallback:car:2025fallbackcarright.webp/v1740000000/common/f1/2025/haas/2025haascarright.webp'/>                    
+                </div>
+                <div className='team-card' id='alpine1'>
+                    <img className='team-logo' src='https://media.formula1.com/image/upload/c_lfill,w_48/q_auto/v1740000000/common/f1/2025/alpine/2025alpinelogowhite.webp'/>
+                    <h4>Alpine</h4>
+                    <img className='team-img' src='https://media.formula1.com/image/upload/c_lfill,h_224/q_auto/d_common:f1:2025:fallback:car:2025fallbackcarright.webp/v1740000000/common/f1/2025/alpine/2025alpinecarright.webp'/>                    
                 </div>
             </div>
         </div>
